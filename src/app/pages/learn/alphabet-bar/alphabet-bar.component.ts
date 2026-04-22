358aac27a1d5580940fd5ba446965e8ca1fbe609
Andrew Faurash <andrew2020wit@gmail.com>
2026-04-22 10:23:12 +0300

    app-alphabet-bar

--- src/app/pages/learn/alphabet-bar/alphabet-bar.component.ts
+++ src/app/pages/learn/alphabet-bar/alphabet-bar.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, computed, input} from '@angular/core';
+
+interface Latter {
+  letter: string;
+  isSelected: boolean;
+}
 
 @Component({
   selector: 'app-alphabet-bar',
@@ -7,5 +12,27 @@ import { Component } from '@angular/core';
   styleUrl: './alphabet-bar.component.scss',
 })
 export class AlphabetBarComponent {
+  public readonly word = input.required<string>();
 
+  private readonly alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  private readonly alphabetArray: string[] = this.alphabet.split('');
+
+  protected readonly letters = computed(()=> {
+    const word = this.word();
+    if (!word) return [];
+
+    const letters: Latter[] = [];
+
+    for (const letter of this.alphabetArray) {
+      letters.push({
+        letter,
+        isSelected: word?.[0].toUpperCase() === letter || word?.[1].toUpperCase() === letter,
+      });
+    }
+
+    console.log(letters);
+
+    return letters;
+  })
 }
